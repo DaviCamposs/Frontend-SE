fa6d9d449eff176a0f0862bf573d0ea3372f9ece
Davi Campos <davi.campos689@gmail.com>
2022-03-05 13:56:53 -0300

    feat: add login e registro page

--- src/app/pages/autenticacao/autenticacao.module.ts
+++ src/app/pages/autenticacao/autenticacao.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AutenticacaoRoutingModule } from './autenticacao-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegistroComponent
+  ],
   imports: [
     CommonModule,
     AutenticacaoRoutingModule
