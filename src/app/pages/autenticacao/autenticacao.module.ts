e794f3dbd97392d8cbf7df0afcb5784ea23ecfb5
Davi Campos <davi.campos689@gmail.com>
2022-03-05 13:44:09 -0300

    feat: add autenticacao module

---
+++ src/app/pages/autenticacao/autenticacao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AutenticacaoRoutingModule } from './autenticacao-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AutenticacaoRoutingModule
+  ]
+})
+export class AutenticacaoModule { }
